ad7d14df287b76fffe0da8b8862beebf24746f12
MariaJose <mariajoserucla@gmail.com>
2025-07-21 12:00:07 +0200

    Table terminada

---
+++ src/app/components/table/table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  templateUrl: './table.html',
+  styleUrls: ['./table.css']
+})
+export class TableComponent {
+
+}
