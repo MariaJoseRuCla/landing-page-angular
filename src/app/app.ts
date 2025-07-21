aed0824ad52b829d7e242723f50c3d4a44fd6b46
MariaJose <mariajoserucla@gmail.com>
2025-07-21 12:08:09 +0200

    parte olvidada de table en app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { WelcomeComponent } from './components/welcome/welcome';
 import { FeaturesComponent } from './components/features/features';
 import { QuestionsComponent } from './components/questions/questions';
 import { GalleryComponent } from './components/gallery/gallery';
+import { TableComponent } from './components/table/table';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent, FeaturesComponent, QuestionsComponent, GalleryComponent],
+  imports: [RouterOutlet, WelcomeComponent, FeaturesComponent, QuestionsComponent, GalleryComponent, TableComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
